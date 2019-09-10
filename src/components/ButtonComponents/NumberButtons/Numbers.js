b08e34b5f610d7f70f7e3082764d0acb1adfd092
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-09-10 16:10:49 -0400

    creates NumberButton components

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import { numbers } from '../../../data';
+import NumberButton from './NumberButton';
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
 /* 
@@ -14,7 +15,13 @@ const Numbers = () => {
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+        numberState.map((numbers, index) => (
+          <NumberButton index={index} numbers={numbers}/>
+        ))
+       }
     </div>
   );
 };
+
+export default Numbers;
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -7,12 +7,17 @@ import { operators } from '../../../data';
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
-  const [numberState, setNumberState] = useState(numbers)
+  const [operatorState, setOperatorState] = useState(operators)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       operatorState.map((operators, index) => (
+         <OperatorButton />
+       ))
+       }
+        
     </div>
   );
 };
